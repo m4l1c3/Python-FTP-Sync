dffd75f8b4ec24aaf3874b857b8efff91ced2d09
Jon Wisdom <jon@jonsomnia.com>
2014-08-02 19:54:41 -0600

    Defined __init__
    Added in definitions to assign to instance variables on the extracor class.

--- Extractor.py
+++ Extractor.py
@@ -1,13 +1,14 @@
-import os, rarfile
+import os
+import imp
+rar_file = imp.load_source('rarfile', '/rarfile/rarfile.py')
 
 class Extractor:
-	sourcePath = ""
-	destPath = ""
+    source_path = ""
+    destination_path = ""
 
-	def __init__(self):
-		return
+    def __init__(self, destination_path, source_path):
+        self.destination_path = destination_path
+        self.source_path = source_path
 
-	def Extract(self):
-		return
-
-	
+    def Extract(self):
+        return
