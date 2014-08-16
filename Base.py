e28a5936a765ea2dc675f2b07ffb3d5f35dd7f78
Jon Wisdom <jon@jonsomnia.com>
2014-08-16 02:41:17 -0600

    Adding base class
    Creating a parent class for handling os specific settings.

---
+++ Base.py
@@ -0,0 +1,35 @@
+import platform
+
+class Base():
+    current_os = ""
+    directory_separator = ""
+
+    def __init__(self):
+        self.current_os = platform.system()
+
+        if self.is_windows():
+            self.directory_separator = "\\"
+        elif self.is_linux() or self.is_osx():
+            self.directory_separator = "/"
+        else:
+            return
+
+    def is_windows(self):
+        if platform.system() is "Windows":
+            return True
+        else:
+            return False
+
+    def is_linux(self):
+        if not platform.system() == "Linux":
+            return True
+        else:
+            return False
+
+    def is_osx(self):
+
+        if not platform.system() == "Darwin":
+            print("here")
+            return True
+        else:
+            return False
