637c3c71a9a3500e6eb9b8f187a4687f34972273
Jon Wisdom <jon@jonsomnia.com>
2014-08-16 02:42:20 -0600

    Adding inheritence
    Adding inheritance from base class to use a property to store how directories are separated by each operating system (linux osx windowz).

--- Extractor.py
+++ Extractor.py
@@ -3,33 +3,39 @@ import imp
 import zipfile
 import re
 import tarfile
+
+from Base import Base
 rar_file = imp.load_source('rarfile', 'rarfile/rarfile.py')
 
 
-class Extractor:
+class Extractor(Base):
     source_path = ""
     destination_path = ""
     zip_regex = re.compile("\.(zip)")
     rar_regex = re.compile("\.(rar)")
 
     def __init__(self, destination_path, source_path):
+        Base.__init__(self)
         self.destination_path = destination_path
         self.source_path = source_path
 
     def extract(self):
-        os.chdir(self.source_path)
-        for f in os.listdir(self.source_path):
-            if self.is_rar_archive(f):
-                self.process_rar_archive(sorted(os.listdir(self.source_path))[0])
-                break
-            elif self.is_zip_archive(f):
-                self.process_zip_archive(sorted(os.listdir(self.source_path))[0])
-            elif self.is_tar_archive(f):
-                self.process_tar_archive(sorted(os.listdir(self.source_path))[0])
+        try:
+            os.chdir(self.source_path)
+            for f in os.listdir(self.source_path):
+                if self.is_rar_archive(f):
+                    self.process_rar_archive(sorted(os.listdir(self.source_path))[0])
+                    break
+                elif self.is_zip_archive(f):
+                    self.process_zip_archive(sorted(os.listdir(self.source_path))[0])
+                elif self.is_tar_archive(f):
+                    self.process_tar_archive(sorted(os.listdir(self.source_path))[0])
+        except Exception as e:
+            print("Error - unable to determine archive type: " + str(e))
 
     def process_tar_archive(self, archive_file):
         try:
-            obj_tar_file = tarfile.open(self.source_path + "/" + archive_file)
+            obj_tar_file = tarfile.open(self.source_path + self.directory_separator + archive_file)
             print("Extracting: " + str(obj_tar_file))
             obj_tar_file.extractall(self.destination_path)
             obj_tar_file.close()
@@ -38,7 +44,7 @@ class Extractor:
 
     def process_rar_archive(self, archive_file):
         try:
-            with rar_file.RarFile(self.source_path + "/" + archive_file) as obj_rar_file:
+            with rar_file.RarFile(self.source_path + self.directory_separator + archive_file) as obj_rar_file:
                 print("Extracting - " + str(obj_rar_file))
                 obj_rar_file.extractall(self.destination_path)
         except Exception as e:
@@ -46,9 +52,9 @@ class Extractor:
 
     def process_zip_archive(self, archive_file):
         try:
-            with zipfile.ZipFile(self.source_path + "/" + archive_file, "r") as zip_file:
+            with zipfile.ZipFile(self.source_path + self.directory_separator + archive_file, "r") as zip_file:
                 print("Extracting - " + zip_file.filename)
-                zip_file.extractall(self.destination_path + "/" + archive_file[:str(archive_file).find(".z")])
+                zip_file.extractall(self.destination_path + self.directory_separator + archive_file[:str(archive_file).find(".z")])
         except Exception as e:
             print("Error - Unable to extract zip: " + str(e))
 
@@ -69,3 +75,7 @@ class Extractor:
             return False
         else:
             return True
+
+
+ext = Extractor("/Users/jonwisdom/Desktop/PythonTest", "/Users/jonwisdom/Desktop/rar")
+ext.extract()
