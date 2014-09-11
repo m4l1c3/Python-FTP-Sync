5e91b25c312315e6e25f75fb1bde4aa827437256
Jon Wisdom <jwisdom@scentsy.com>
2014-09-11 15:49:11 -0600

    Refactoring the stuff from environment variables to be in base so they're available to all the child classes.

--- Base.py
+++ Base.py
@@ -1,14 +1,30 @@
 import platform
+import os
 
 
 class Base():
     current_os = ""
     directory_separator = ""
+    ftp_user = ""
+    ftp_password = ""
+    ftp_server = ""
+    ftp_port = 21
+    remote_directory_to_sync = ""
+    local_directory_to_sync = ""
     # logger = Logger("Initializing Base")
 
     def __init__(self):
         self.current_os = platform.system()
+        self.set_directory_separator()
+        self.ftp_server = os.environ["FtpSyncServer"]
+        self.ftp_user = os.environ["FtpSyncUser"]
+        self.ftp_password = os.environ["FtpSyncPassword"]
+        self.ftp_port = os.environ["FtpSyncPort"]
+        self.remote_directory_to_sync = os.environ["FtpSyncRemoteDirectory"]
+        self.local_directory_to_sync = os.environ["FtpSyncLocalDirectory"]
 
+
+    def set_directory_separator(self):
         if self.is_windows():
             self.directory_separator = "\\"
         elif self.is_linux() or self.is_osx():
