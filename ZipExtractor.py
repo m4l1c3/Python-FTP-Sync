2bb65c47d6e35a64e5c41c695e7bb8c38f04aadc
Jon Wisdom <jon@jonsomnia.com>
2014-12-18 22:57:56 -0700

    Adding zip object
    Encapsulating zip stuff into a class.

---
+++ ZipExtractor.py
@@ -0,0 +1,30 @@
+import zipfile
+from Base import Base
+
+
+class ZipArchive(Base):
+    source_directory = ""
+    destination_directory = ""
+    archive_file = ""
+
+    def __init__(self, source_directory, destination_directory, archive_file):
+        Base.__init__(self)
+        self.source_directory = source_directory
+        self.destination_directory = destination_directory
+        self.archive_file = archive_file
+        self.process_zip_archive()
+
+    def process_zip_archive(self):
+        try:
+            with zipfile.ZipFile(self.source_directory + self.directory_separator + self.archive_file, "r") as zip_file:
+                print("Extracting - " + zip_file.filename)
+                zip_file.extractall(self.destination_path + self.directory_separator +
+                                    self.archive_file[:str(self.archive_file).find(".z")])
+        except Exception as e:
+            print(str(e))
+
+    def is_zip_archive(self, target_file_name):
+        if not zipfile.is_zipfile(target_file_name):
+            return False
+        else:
+            return True
