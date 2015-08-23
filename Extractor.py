f02991f66490c1f0f90476cc9bd8126da3fce28e
Jon Wisdom <jon@jonsomnia.com>
2015-08-22 21:04:43 -0600

    Fixing exceptions

--- Extractor.py
+++ Extractor.py
@@ -26,45 +26,51 @@ class Extractor(Base):
         try:
             os.chdir(self.extraction_path)
             for folder in os.listdir(self.extraction_path):
-                Logger("Looping - folder: " + folder)
-
                 try:
-                    for file_to_check in os.listdir(self.extraction_path +
-                            self.directory_separator + folder):
-                        if self.is_rar_archive(self.extraction_path +
-                                self.directory_separator + folder +
-                                self.directory_separator + file_to_check):
-                            Logger("Status - UnRar Archive: " +
-                                   self.extraction_path +
-                                   self.directory_separator +
-                                   folder)
-                            RarExtractor(self.extraction_path +
-                                         self.directory_separator +
-                                         folder,
-                                         file_to_check)
-                        elif self.is_zip_archive(self.extraction_path +
-                                self.directory_separator +
-                                folder +
-                                self.directory_separator +
-                                file_to_check):
-                            Logger("Status - Attempting to extract ZIP Archive in: " + self.extraction_path)
-                            ZipArchive(self.extraction_path + self.directory_separator + folder,
-                                    sorted(os.listdir(self.extraction_path + self.directory_separator +
-                                                      file_to_check))[0])
-                        elif self.is_tar_archive(self.extraction_path + self.directory_separator + folder + 
-                                self.directory_separator + file_to_check):
-                            Logger("Status - Attempting to extract TAR Archive in: " + self.extraction_path)
-                            TarArchive(self.extraction_path + self.directory_separator + folder,
-                                    sorted(os.listdir(self.extraction_path + self.directory_separator +
-                                                      file_to_check))[0])
-                        else:
-                            continue
+                    if os.path.isdir(self.extraction_path +
+                                self.directory_separator + folder):
+                        Logger("Looping - folder: " + folder)
+                        for file_to_check in os.listdir(self.extraction_path +
+                                self.directory_separator + folder):
+                            full_path = self.extraction_path + self.directory_separator + folder + self.directory_separator \
+                                        + file_to_check
+                            if not os.path.isdir(full_path) and not self.acceptable_file_types.search(file_to_check) \
+                                and not self.disallowed_file_types.search(file_to_check)  :
+                                self.check_for_extractable_item(folder, file_to_check)
+                            elif os.path.isdir(file_to_check):
+                                for sub_folder_file_to_check in os.listdir(full_path):
+                                    if not os.path.isdir(full_path + self.directory_separator + sub_folder_file_to_check):
+                                        self.check_for_extractable_item(folder + self.directory_separator
+                                            + file_to_check, sub_folder_file_to_check)
+                            else:
+                                continue
+                    else:
+                        continue
                 except Exception as exception:
                     Logger("Error - checking archive: " + str(exception))
                     continue
         except Exception as exception:
             Logger("Error - unable to determine archive type: " + str(exception))
-    
+
+    def check_for_extractable_item(self, parent_folder, file_to_check):
+        parent_folder_path = self.extraction_path + self.directory_separator + parent_folder
+        full_file_path = self.extraction_path + self.directory_separator + parent_folder + self.directory_separator \
+                         + file_to_check
+        if self.is_rar_archive(full_file_path):
+            Logger("Status - UnRar Archive: " + full_file_path)
+            RarExtractor(parent_folder_path, full_file_path)
+            return True
+        else:
+            if self.is_zip_archive(full_file_path):
+                Logger("Status - Attempting to extract ZIP Archive: " + full_file_path)
+                ZipArchive(parent_folder_path, sorted(os.listdir(parent_folder))[0])
+                return True
+            if self.is_tar_archive(full_file_path):
+                Logger("Status - Attempting to extract TAR Archive in: " + full_file_path)
+                TarArchive(parent_folder_path, sorted(os.listdir(parent_folder_path))[0])
+                return True
+        return False
+
     @staticmethod
     def is_zip_archive(target_file_name):
         if not zipfile.is_zipfile(target_file_name):
